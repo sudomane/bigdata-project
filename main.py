cc7c93dd50a03f9255da40184ab2caff8031a72e
sudomane <philippebouchet13@gmail.com>
2022-03-13 20:46:22 +0100

    Fixed a few typos, and changed variable names for clarity.

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import pandas as pd
 how_to_use = "Usage: main.py [path to excel database]"
 
 arguments = len(sys.argv)
+parameter = sys.argv[1]
 
-if (arguments == 2 and sys.argv[1] == 'h'):
+if (arguments == 2 and (parameter == 'h' or parameter == "help")):
     print(how_to_use)
     exit(1)
 
 # URL to RATP Excel dataset
 dataset = "https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2020/download/?format=xls&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true"
 
-# If no arguments are provided, use default dataset.
+# If arguments are provided, use path to dataset. If not, URL is used by default.
 if (arguments == 2):
-    dataset = str(sys.argv[1])
+    dataset = parameter
     
 # Throw exception if we couldn't load the file, or URL is invalid.
 try:
