96d459a11cc54dcc534136ce5290334928c97e12
sudomane <43009761+sudomane@users.noreply.github.com>
2022-03-13 22:34:25 +0100

    Update test_isvaliddata.py

--- tests/test_isvaliddata.py
+++ tests/test_isvaliddata.py
@@ -2,6 +2,9 @@ import pytest
 
 url = "https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2020/download/?format=json&timezone=Europe/Berlin&lang=fr"
 
+# Simply checking if the column "Rang" is in the dataframe.
+# If it is, then the dataframe has been correctly loaded.
+# If not, then we need to properly clean the data.
 def test_isvaliddata():
     dataframe = loadData(url)
     assert "Rang" in dataframe
