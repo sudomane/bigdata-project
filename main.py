4327b777f9ecdec086c0765bf4f61067d7a189ee
sudomane <philippebouchet13@gmail.com>
2022-03-13 20:44:19 +0100

    Proper parameter handling. Added help to describe how to use the program.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sys
 import pandas as pd
 
+how_to_use = "Usage: main.py [path to excel database]"
+
+arguments = len(sys.argv)
+
+if (arguments == 2 and sys.argv[1] == 'h'):
+    print(how_to_use)
+    exit(1)
+
 # URL to RATP Excel dataset
 dataset = "https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2020/download/?format=xls&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true"
 
 # If no arguments are provided, use default dataset.
-if (len(sys.argv) == 2):
+if (arguments == 2):
     dataset = str(sys.argv[1])
     
 # Throw exception if we couldn't load the file, or URL is invalid.
