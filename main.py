37c679295976784199b7ccfdc512117be552471c
sudomane <philippebouchet13@gmail.com>
2022-03-13 21:45:12 +0100

    Switched from Excel to JSON

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ if (arguments == 2 and (sys.argv[1] == 'h' or sys.argv[1] == "help")):
     print(how_to_use)
     exit(1)
 
-# URL to RATP Excel dataset
-dataset = "https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2020/download/?format=xls&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true"
+# URL to RATP JSON dataset    
+dataset = "https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2020/download/?format=json&timezone=Europe/Berlin&lang=fr"
 
 # If arguments are provided, use path to dataset. If not, URL is used by default.
 if (arguments == 2):
     dataset = sys.argv[1]
-    
+
 # Throw exception if we couldn't load the file, or URL is invalid.
 try:
-    dataframe = pd.read_excel(dataset)
+    dataframe = pd.read_json(dataset)
 except Exception as e:
     print(e)
