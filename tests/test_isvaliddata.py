817ddfa4bd8493184e48fdf0773b234a29d22590
sudomane <philippebouchet13@gmail.com>
2022-03-15 21:17:55 +0100

    Fixed test bug

--- tests/test_isvaliddata.py
+++ tests/test_isvaliddata.py
@@ -6,12 +6,12 @@ from src.dataprocessing import getLineInfo
 
 url = "https://data.ratp.fr/explore/dataset/trafic-annuel-entrant-par-station-du-reseau-ferre-2020/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B"
 dataframe = loadData(url)
-dict = getLineInfo(dataframe, 14)
+dict = getLineInfo(dataframe, "14")
 # Simply checking if the column "Rang" is in the dataframe.
 # If it is, then the dataframe has been correctly loaded.
 # If not, then we need to properly clean the data.
 def test_isvaliddata():
-    assert "Stations" in dataframe
+    assert "Station" in dataframe
 
 def test_isvalidinfo():
     assert dict["BIBLIOTHEQUE"] == 10002396
