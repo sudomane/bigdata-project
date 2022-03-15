edea9781e04771d4ba8599ac15340b2ce33e639f
sudomane <philippebouchet13@gmail.com>
2022-03-15 14:00:36 +0100

    Added exact color scheme for every Metro and RER lines

--- src/main.py
+++ src/main.py
@@ -23,7 +23,31 @@ if (arguments == 2):
 dataframe = dp.loadData(dataset)
 display(dataframe)
 
-dataframe.hist(column='Trafic', bins=26, grid=False, figsize=(12,8), color='#86bf91', zorder=2, rwidth=0.9)
+colors = {
+    "1" : "#fecd04",
+    "2" : "#006cb8",
+    "3" : "#9b983b",
+    "3bis" : "#89d3de",
+    "4" : "#bd499c",
+    "5" : "#f68d49",
+    "6" : "#76c595",
+    "7" : "#eb96a6",
+    "7bis" : "#76c596",
+    "8" : "#c3a0ca",
+    "9" : "#cdc729",
+    "10" : "#deaf39",
+    "11" : "#8d6438",
+    "12" : "#008c59",
+    "13" : "#86cfdb",
+    "14" : "#642c8f",
+    "A" : "#ff1400",
+    "B" : "#3c91dc",
+    "C" : "#ffbe00",
+    "D" : "#00643c",
+    "E" : "#a0006e",
+}
+
+dataframe.hist(column='Trafic', bins=26, grid=False, figsize=(12,8), color=colors["1"], zorder=2, rwidth=0.9)
 trafic = dataframe['Trafic'].tolist()
 stations = dataframe['Station'].tolist()
 # x = range(1, len(stations) +1)
