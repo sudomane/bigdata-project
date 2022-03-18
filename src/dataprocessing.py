6af07b463c6be6e28daecb5266cadbc10d2b2425
sudomane <philippebouchet13@gmail.com>
2022-03-18 13:06:33 +0100

    Properly load in dataset

--- src/dataprocessing.py
+++ src/dataprocessing.py
@@ -9,8 +9,21 @@ path = os.path.join(path, "*.csv")
 
 files = glob.glob(path)
 
-print(files)
-
 def loadData() -> pd.DataFrame:
+    # Read every CSV file, load it into one dataframe
     dataframe = pd.concat((pd.read_csv(f, sep=';', on_bad_lines='skip') for f in files))
+    
+    # Remove useless columns
+    dataframe.drop(columns=["CNIT", 
+                            "Lieu Admin Actuel - Territoire Nom", 
+                            "Lettre Conventionnelle Véhicule", 
+                            "Id_accident"], 
+                   inplace=True)
+    
+    # Rename column for better readability
+    dataframe.rename(columns={"Type Accident - Libellé" : "Type Accident"}, inplace=True)
+    
+    # Remove redundant "Accident" prefix in every row
+    dataframe["Type Accident"].replace(to_replace="Accident *", value = "", regex=True, inplace=True)
+    
     return dataframe
