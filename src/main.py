40d36cb4f3a3974839e17016689e275583dc2ae9
sudomane <philippebouchet13@gmail.com>
2022-03-18 15:20:37 +0100

    Using plotly to plot the total accidents per year

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
 import dataprocessing as dp
 
 dataframe = dp.loadData()
+print("Loaded database!")
+
+# TODO: Find a way to select accident type within group
+sum = dataframe.groupby('Année').size()
+
+fig = sum.plot()
+fig.show()
