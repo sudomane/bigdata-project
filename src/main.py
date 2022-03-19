7462d739c723c73770bc567bb04f135341fe8fcc
spurdo <spurdo@rev.rev>
2022-03-19 10:18:59 +0100

    Cleaning up main.

--- src/main.py
+++ src/main.py
@@ -6,21 +6,3 @@ print("Loaded database!")
 
 totalaccidents_2010 = dp.getInfo(df, 2010, accidentType="mortel",vehicleType="PL",age=20)
 print(totalaccidents_2010)
-#fig = px.line(df, x="Année", y="Type Accident", title="Accidents en France 2010 - 2020")
-#fig.show()
-
-#light_accident = dataframe["Type Accident"] == "Léger"
-#fatal_accident = dataframe["Type Accident"] == "mortel"
-#nonfatal_accident = dataframe["Type Accident"] == "grave non mortel"
-
-# TODO: Find a way to select accident type within group
-#sum = dataframe.groupby('Année')
-
-#light = dataframe[light_accident].groupby('Année').size()
-#fatal = dataframe[fatal_accident].groupby('Année').size()
-#nonfatal = dataframe[nonfatal_accident].groupby('Année').size()
-
-#fig = light.plot()
-#fig = fatal.plot()
-#fig = nonfatal.plot()
-#fig.show()
