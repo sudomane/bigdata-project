adc283cd0691d1a60f9c8371dc282a1baff8747b
sudomane <43009761+sudomane@users.noreply.github.com>
2022-03-20 15:42:17 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -21,14 +21,6 @@ print("Loaded database in " + "{:.2f}".format(t_2 - t_1) + " seconds!")
 # - grave non mortel
 # - Léger
 
-t_1 = time.time()
-# Demonstration on how to use the function
-totalaccidents = dp.getInfo(df, vehicleType="TC")
-t_2 = time.time()
-
-print("Fetched data in " + "{:.2f}".format(t_2 - t_1) + " seconds!")
-print(totalaccidents)
-
 # TODO: Dynamically display different vehicle types, and accident types
 # NOTE: Refer to Chapter 23 - Dynamic Graphs with Plotly -- Subplots, in docker courses, check unemployment graph for more info
 fig = px.histogram(
