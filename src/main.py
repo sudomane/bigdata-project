9f6aa022de57e6df53c9462704c95fe6e2192521
MiChatz <michalis@hey.com>
2022-03-29 23:28:12 +0200

    added mortality / vehicule age 3D graph for every year

--- src/main.py
+++ src/main.py
@@ -29,45 +29,26 @@ print("Loaded database in " + "{:.2f}".format(t_2 - t_1) + " seconds!")
 # TODO: Dynamically display different vehicle types, and accident types
 # NOTE: Refer to "Aggregation" for more information. Check plotly documentation
 
-# figure = px.histogram(
-#         dp.getInfo(df=df), 
-#         x=df["Année"],
-#         color = df["Catégorie véhicule"],
-#         facet_col = "Type Accident", 
-#         facet_col_wrap = 3,
-# )
-# figure.update_layout(barmode="stack", bargap=0.2)
-# figure.show()
-
-df2 = dp.getInfo(df=df)
-res = df2.groupby(["Age véhicule", "Catégorie véhicule", "Type Accident", "Année"])
-mdf = pd.DataFrame({'Age': [], 'V. Type': [], 'A. Type': [], 'Year': [], 'Count': []})
-for i, g in enumerate(res):
-    mdf.loc[i] = [g[0][0], g[0][1], g[0][2], g[0][3], g[1].size]
-
-clr = {"Léger": "#4a1c40", "mortel": "#2b8b27", "grave non mortel": "#d18502"}
-
-trace = go.Scatter3d(
-    x = mdf['Count'],
-    y = mdf['Age'],
-    z = mdf['Year'],
-    mode = 'markers',
-    marker = dict( 
-                  color = mdf['A. Type'].apply(lambda x: clr[x]),
-        opacity = 0.8),
-
+figure1 = px.histogram(
+        dp.getInfo(df=df),
+        x=df["Année"],
+        color = df["Catégorie véhicule"],
+        facet_col = "Type Accident",
+        facet_col_wrap = 3,
 )
+figure1.update_layout(barmode="stack", bargap=0.2)
+figure1.show()
 
-layout2 = go.Layout(scene = {'aspectmode':'cube'})
-layout2['scene'].update(xaxis = {'title':'Age'}, yaxis = {'title':'Height'}, zaxis = {'title':'Rank'})
-layout2['title'] = 'Number of accidents per year. The '
+mdf = dp.getCounts(df=df)
+clr = {"Léger": "#9ecbed", "mortel": "#2a6a99", "grave non mortel": "#3c97da"}
 
-camera = dict( up=dict(x=1, y=0, z=0),           # determine the up direction on the page (here x i.e. Age)
-               center=dict(x=0, y=0, z=0),       # (0,0,0) is always the center of the domain, no matter data values
-               eye=dict(x=0.25, y=1.7, z=-1.2)   # x 2 to value unzoom by 2,  < 1 and you are in the domaine
-             )
+figure2 = px.scatter_3d(
+    mdf,
+    x = 'Année',
+    y = 'Age véhicule',
+    z = 'Count',
+    color = 'Type Accident',
+    color_discrete_map=clr,
+) 
 
-layout2['scene_camera'] = camera
-
-fig = go.Figure(data=[trace], layout=layout2)
-py.iplot(fig, show_link=False)
+figure2.show()
